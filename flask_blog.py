df9fbbec1e3eab263ed45ac40d3113516dbf78a8
Byron Glaspy <byronglaspy@byrons-mbp.attlocal.net>
2019-10-30 01:33:18 -0400

    add layout template

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'J.K. Rowling',
+        'title': 'Fantastic Beasts and Where to Find Them',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'J.R.R. Tolkien',
+        'title': 'The Fall of Gondolin',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts  = posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
     
 
 if __name__ == '__main__':
